1d484e96d77e4524efb10f9cae0e0b40f5de7ac9
Deepansh Khurana <deepanshkhurana@outlook.com>
2020-05-01 01:52:52 +0530

    Add string_reverser.py

---
+++ code_examples/string_reverser.py
@@ -0,0 +1,14 @@
+def string_reverser(our_string):
+
+    """
+    Reverse the input string
+
+    Args:
+       our_string(string): String to be reversed
+    Returns:
+       string: The reversed string
+    """
+    
+    # TODO: Write your solution here
+    
+    return(our_string[::-1])
