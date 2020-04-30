75b1e77c81e10f8e3d99b30d4e439362aee64237
Deepansh Khurana <deepanshkhurana@outlook.com>
2020-05-01 01:53:13 +0530

    Add word_flipper.py

---
+++ code_examples/word_flipper.py
@@ -0,0 +1,37 @@
+def word_flipper(our_string):
+
+    """
+    Flip the individual words in a sentence
+
+    Args:
+       our_string(string): String with words to flip
+    Returns:
+       string: String with words flipped
+    """
+    
+    tokens = our_string.split(" ")
+    new_tokens = []
+    
+    for token in tokens:
+            new_tokens.append(token[::-1])
+    
+    return(" ".join(new_tokens))
+    
+    
+def word_flipper_udacity(our_string):
+
+    """
+    Flip the individual words in a sentence
+
+    Args:
+       our_string(string): Strings to have individual words flip
+    Returns:
+       string: String with words flipped
+    """
+
+    word_list = our_string.split(" ")
+
+    for idx in range(len(word_list)):
+        word_list[idx] = word_list[idx][::-1]
+
+    return " ".join(word_list)
